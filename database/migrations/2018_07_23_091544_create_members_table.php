e71dfc5a782ab8da138e41d7a46225a38b0b2860
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-07-23 21:16:24 +0600

    Member migration and model add

---
+++ database/migrations/2018_07_23_091544_create_members_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nid', 30);
+            $table->string('name', 200);
+            $table->string('fatherName', 200);
+            $table->string('motherName', 200);
+            $table->string('husWifeName', 200);
+            $table->text('presentAddress');
+            $table->text('permanentAddress');
+            $table->string('dob', 20);
+            $table->string('education', 200);
+            $table->string('nationality', 30);
+            $table->string('gender', 10);
+            $table->string('bloodGroup', 5);
+            $table->string('mobileNo', 15);
+            $table->string('email', 100);
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
